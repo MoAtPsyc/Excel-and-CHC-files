9fd9710f54b97b2603aa2f41c7d257f8537e5db8
MoAtPsyc <moshirabbi@gmail.com>
2025-04-06 12:29:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('downloadBtn').addEventListener('click', function () {
   const link = document.createElement('a');
   link.href = 'https://raw.githubusercontent.com/your-username/your-repo-name/main/your-file.xlsx'; // Modify URL below with your actual file path
-  link.download = 'myfile.xlsx';  // You can name the file whatever you want
+  link.download = 'CHC_Scoringtable.docx';  
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
