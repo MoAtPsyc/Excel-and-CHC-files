52105a9797a9ca7ed7e5be502a92794b89dd43bf
MoAtPsyc <moshirabbi@gmail.com>
2025-04-06 12:32:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('downloadBtn').addEventListener('click', function () {
   const link = document.createElement('a');
-  link.href = 'https://raw.githubusercontent.com/your-username/your-repo-name/main/your-file.xlsx'; // Modify URL below with your actual file path
+  link.href = 'https://raw.githubusercontent.com/MoAtPsyc/Excel-and-CHC-files/main/CHC_Scoringtable.docx'; 
   link.download = 'CHC_Scoringtable.docx';  
   document.body.appendChild(link);
   link.click();
